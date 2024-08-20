b1fd58fa800229779ca4c70715b145c5e4f64c59
Manager450 <dandee060@gmail.com>
2024-08-20 21:17:33 +0000

    register resolved

--- BusTicketX/settings.py
+++ BusTicketX/settings.py
@@ -146,6 +146,8 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'your-email@example.com'
 EMAIL_HOST_PASSWORD = 'your-email-password'
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
 TWILIO_ACCOUNT_SID = 'your-twilio-account-sid'
 TWILIO_AUTH_TOKEN = 'your-twilio-auth-token'
 TWILIO_PHONE_NUMBER = 'your-twilio-phone-number'
